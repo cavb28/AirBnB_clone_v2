c636b940fad80f2a4f4d406767af54af110566dd
Cesar Velez <cavb.28@gmail.com>
2020-01-03 22:09:22 -0500

    task 3: full deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,73 +1,73 @@
 #!/usr/bin/python3
-"""
-creates and distributes an archive to your web servers,
-using the function deploy
-"""
+"""Distributes an archive to the web servers."""
 
 from datetime import datetime
 from fabric.operations import local, put, run
 from fabric.api import env
 from os import path
+import ntpath
 
-env.hosts = ['35.243.220.111', '35.190.130.57']
+env.hosts = ['35.227.38.129', '35.227.12.193']
 
 
 def do_pack():
-    """
-    Tars every file in web_static into versions
-    """
+    """Function to generate version compressed files"""
     local("mkdir -p versions")
-    my_path = local("tar -zcvf versions/web_static_{}.tgz web_static".format(
+    path = local("tar -zcvf versions/web_static_{}.tgz web_static".format(
         datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
 
-    my_path_name = "versions/web_static_{}.tgz".format(
-        datetime.strftime(datetime.now(), "%Y%m%d%H%M%S"))
-
-    if my_path.failed:
+    if path.failed:
         return None
-    return my_path_name
+    return path
 
 
 def do_deploy(archive_path):
-    """
-    deploy archive
-    :param archive_path:
-    :return:
-    """
+    """deploy archive"""
     if not path.exists(archive_path):
         return False
 
     try:
-        my_name = archive_path[9:]
-        my_name_short = archive_path[9:-4]
-        put(archive_path, "/tmp/{}".format(my_name))
-        run("mkdir -p /data/web_static/releases/{}/".format(my_name_short))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(my_name, my_name_short))
-        run("mv /data/web_static/releases/{}/web_static/*\
+        head, tail = ntpath.split(archive_path)
+        if tail:
+            file = tail
+        else:
+            file = ntpath.basename(head)
+
+        head, tail = ntpath.splitext(file)
+        if head:
+            name = head
+        else:
+            name = ntpath.basename(head)
+
+        put(archive_path, "/tmp/{}".format(file))
+
+        run("sudo mkdir -p /data/web_static/releases/{}/".format(name))
+        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(file, name))
+        print(file)
+        run("sudo mv /data/web_static/releases/{}/web_static/*\
                             /data/web_static/releases/{}/"
-            .format(my_name_short, my_name_short))
-        run("rm /tmp/{}".format(my_name))
-        run("rm -fr /data/web_static/current")
-        run("rm -fr /data/web_static/releases/{}/web_static"
-            .format(my_name_short))
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(my_name_short))
+            .format(name, name))
+
+        run("sudo rm /tmp/{}".format(file))
+
+        run("sudo rm -rf /data/web_static/current")
+
+        run("sudo rm -rf /data/web_static/releases/{}/web_static"
+            .format(name))
+        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(name))
+
         print("New version deployed!")
+
     except Exception:
         return False
 
 
 def deploy():
-    """
-    Full deployment
-    :return:
-    """
+    """Full deployment"""
 
-    """Call the do_pack() function and store the path of the created archive"""
-    my_pack = do_pack()
-    """ Return False if no archive has been created"""
-    if not my_pack:
+    pack = do_pack()
+    if not pack:
         return False
-    """ Return the return value of do_deploy"""
-    return do_deploy(my_pack)
+    return do_deploy(pack)
