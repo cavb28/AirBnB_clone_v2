307c62170c8fd8c237c61bcf393fba6e3c25b1f0
Cesar Velez <cavb.28@gmail.com>
2020-01-03 20:10:32 -0500

    task 1: fabric file to generate tar.gz

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
-"""
-Generates a .tgz archive from the contents of the web_static folder
-of the AirBnB Clone repo, using the function do_pack.
-"""
+"""Generates a .tgz archive from web_static folder,
+using the function do_pack."""
 from datetime import datetime
 from fabric.operations import local
 
 
 def do_pack():
-    """
-    Tars every file in web_static into versions
-    """
-    """Create local directory versions"""
-    local("mkdir -p versions")
-    """execute te tar command locally tar -zcvf
-    use datetime for the file name
-    """
-    my_path = local("tar -zcvf versions/web_static_{}.tgz web_static".format(
+    """Tars files in web_static into versions"""
+    path = local("tar -cvzf versions/web_static_{}.tgz web_static".format(
         datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
 
-    if my_path.failed:
+    if path.failed:
         return None
-    return my_path
+    return path
