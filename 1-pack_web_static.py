31e78442afff97935c626017a7a90f2b0387e08e
Cesar Velez <cavb.28@gmail.com>
2020-01-03 20:21:46 -0500

    task 1: fabric file to generate tar.gz. fixed

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""Generates a .tgz archive from web_static folder,
-using the function do_pack."""
+"""Generates a .tgz archive from web_static folder."""
 from datetime import datetime
 from fabric.operations import local
 
 
 def do_pack():
-    """Tars files in web_static into versions"""
-    path = local("tar -cvzf versions/web_static_{}.tgz web_static".format(
+    """Function to generate version compressed files"""
+    path = local("tar -cavf versions/web_static_{}.tgz web_static".format(
         datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
 
     if path.failed:
