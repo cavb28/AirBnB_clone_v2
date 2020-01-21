65e98f9dd72887e5fe446b346fb5b6f04b941d1c
Cesar Velez <cavb.28@gmail.com>
2020-01-21 16:38:50 -0500

    task 9: States and cities connection

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Starts a Flask web application:."""
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def app_context(abc):
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def states_list():
+    states = storage.all('State')
+    return render_template('8-cities_by_states.html', data=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
